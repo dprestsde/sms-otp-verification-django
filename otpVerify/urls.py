e1d76ea0343f7544463514522648c3f5bd0ee649
Deepak-venkayya-naidu-puppala <17pa1a1248@vishnu.edu.in>
2019-12-05 10:45:25 +0530

    view for sending otp

--- otpVerify/urls.py
+++ otpVerify/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import sendOTPView
+
 urlpatterns = [
-   
+    path('send-otp/', sendOTPView.as_view()),
 
 ]
--- otpVerify/views.py
+++ otpVerify/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from django.http import JsonResponse
+import requests
+
+from rest_framework import status
+
+
+''' 
+   We are using 2factor api for otp verification.
+    for more info about please goto https://www.2factor.in
+'''
+class sendOTPView(APIView):
+    def post(self, request):
+        if request.method == "POST":
+            phone_number = request.POST["ph_num"]
+
+            api_key = "f79adac9-1711-11ea-9fa5-0200cd936042" # an unique api key to get authorization
+                                                            # to access 2factor api
+
+            url = "http://2factor.in/API/V1/"+ api_key+ "/SMS/" + phone_number +"/AUTOGEN/OTPSEND"
+            print(url)
+            try:
+                response = requests.request("GET", url)
+                message =  {"status": "Sucessfully otp has generated"}
+            except:
+                message = {"status" : "OTP generation failed"}
+                return JsonResponse(message, status=status.HTTP_404_NOT_FOUND)
+            data = response.json()
+            print(data)
+            request.session["otp_details"] = data["Details"]   # OTP data is stored in session for further verification
+            return JsonResponse(message, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse("Only post method is allowed", safe=False, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+            
+
